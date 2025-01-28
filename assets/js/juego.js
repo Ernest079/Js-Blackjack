953807685c86bbb08ece2d456e2dd8b430b1ebeb
Ernest079 <ernestomendoza.martinez73@gmail.com>
2025-01-28 11:57:26 -0600

    Pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+//Make a new deck
 const crearDeck = () => {
     for (let i = 2; i<=10; i++) {
         for ( let tipo of tipos) {
@@ -21,7 +22,7 @@ const crearDeck = () => {
         }
     }
 
-    console.log(deck);
+    // console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
@@ -29,3 +30,38 @@ const crearDeck = () => {
 
 
 crearDeck();
+
+// This function allow take a card
+const pedirCarta = () => {
+
+    if(deck.length === 0){
+        throw 'NO hay mas cartas';
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta);
+    return carta;
+}
+
+//pedirCarta();
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ? 
+            (valor === 'A') ? 11 : 10 
+            : valor * 1;
+                            
+
+    // if(isNaN(valor)){
+    //     puntos = (valor === 'A') ? 11 : 10;
+    // } else {
+    //     puntos = valor * 1;
+    // }
+
+    // console.log(puntos);
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
